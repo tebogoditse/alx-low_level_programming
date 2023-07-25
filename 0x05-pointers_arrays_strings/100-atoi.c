8d71c0e61708481e48b0042282ff67f225f426a3
tebogoditse <tebogoditse2@gmail.com>
2023-07-25 04:33:34 +0200

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+int	_atoi(char *s)
+{
+	int sum, found, sign;
+
+	sum = 0;
+	sign = 1;
+	found = 1;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\f' || str[i] == '\r')
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i] && found)
+	{
+		if (str[i] > '-1' && str[i] < '10')
+			sum = sum * 10 + *str - '0';
+		else
+			found = 0;
+		i++;
+	}
+	return (sign * sum);
+}
