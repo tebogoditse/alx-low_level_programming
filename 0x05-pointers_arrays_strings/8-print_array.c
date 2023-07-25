d4a4aef0314c5a6234f16c2b3f7ae2d29d5e6b64
tebogoditse <tebogoditse2@gmail.com>
2023-07-25 04:01:03 +0200

    8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,10 +14,9 @@ void	print_array(int *a, int n)
 	i = 0;
 	while (i < n)
 	{
-		if (i == 0)
-			printf("%d", a[i]);
-		else
-			printf(", %d", a[i]);
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
 		i++;
 	}
 	_putchar('\n');
