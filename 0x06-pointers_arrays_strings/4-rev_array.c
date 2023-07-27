40980fcbc6452aed5184b3a931ff4ced8de32e12
tebogoditse <tebogoditse2@gmail.com>
2023-07-28 01:36:32 +0200

    task4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void	reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	i = n - 1;
+	while (i >= n / 2)
+	{
+		temp = a[n - 1 - i];
+		a[n - 1 - i] = a[i];
+		a[i] = temp;
+	}
+}
