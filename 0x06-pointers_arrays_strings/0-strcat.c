561fe36b32ffbfba9e3e097926124fbf3730acc0
tebogoditse <tebogoditse2@gmail.com>
2023-07-27 18:50:59 +0200

    task0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+char	*_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+
+	j = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
