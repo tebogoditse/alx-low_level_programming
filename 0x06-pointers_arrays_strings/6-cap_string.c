a114673ff1b581bf0e5214d3bdd0083b2e55c02d
tebogoditse <tebogoditse2@gmail.com>
2023-07-28 02:01:52 +0200

    task6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+char	*cap_string(char *s)
+{
+	int i, j;
+
+	char s_chars = {' ', '\t', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}', '\n'};
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+		j = 0;
+		while (j <= 12)
+		{
+			if (s[i] == s_chars[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] = s[i + 1] - 32;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
