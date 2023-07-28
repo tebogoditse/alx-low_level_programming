286a69b77f28a606475399253012368545c904e2
tebogoditse <tebogoditse2@gmail.com>
2023-07-28 03:07:15 +0200

    bonus2

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,16 +8,20 @@
 
 void	print_number(int n)
 {
+	unsigned int n1;
+
+	n1 = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		n1 = -n;
 	}
-	if (n > 9)
+	if (n1 > 9)
 	{
-		_putchar(n / 10);
-		n = n % 10;
+		_putchar(n1 / 10);
+		n1 = n1 % 10;
 	}
-	if (n < 10)
-		_putchar(n + 48);
+	if (n1 < 10)
+		_putchar(n1 + 48);
 }
