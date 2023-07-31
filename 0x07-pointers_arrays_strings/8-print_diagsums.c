cc3622cccbec9b9a1989693e07d4f45ede360858
tebogoditse <tebogoditse2@gmail.com>
2023-07-31 21:55:42 +0200

    8-print_diagsums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+void	print_diagsums(int *a, int size)
+{
+	int	i, j, sum1, sum2, k, l;
+
+	k = size - 1;
+	l = 0;
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size)
+		{
+			if (i == j)
+				sum1 = sum1 + a[l];
+			if (j == k)
+				sum2 = sum2 + a[l];
+			l++;
+			j++;
+		}
+		k--;
+		i++;
+	}
+	printf("%i, %i\n", sum1, sum2);
+}
