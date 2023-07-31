dae646ee2ffef5f3d795f7fabf7f62db2884e6a5
tebogoditse <tebogoditse2@gmail.com>
2023-07-31 17:51:22 +0200

    5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+char	*strstr(char *haystack, char *needle)
+{
+	int	i, j;
+
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while (haystack[i + j] != '\0' && haystack[i + j] == needle[j])
+		{
+			if (needle[j + 1] == '\0')
+				return (&str[i]);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
