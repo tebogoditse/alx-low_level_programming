f3a552462c0c8fb6b29c2289a18b32085d4f4592
tebogoditse <tebogoditse2@gmail.com>
2023-07-31 17:34:17 +0200

    hopefully

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+char	*_strpbrk(char *s, char *accept)
+{
+	int	i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[k])
+				return (s);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
