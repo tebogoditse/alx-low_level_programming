7e9016a2954474f66a188c873227ade759f514ce
tebogoditse <tebogoditse2@gmail.com>
2023-07-31 15:14:29 +0200

    modify 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,21 @@
 #include "main.h"
 
-char	*_memset(char *dest, char src, unsigned int n)
+/**
+ * *_memset - sets the first n bytes to the value c
+ * @dest: pointer to destination
+ * @c: character to set
+ * @n: number of bytes to set to the value
+ * Return: the value of dest
+ */
+
+char	*_memset(char *dest, char c, unsigned int n)
 {
 	unsigned int	i;
 
 	i = 0;
 	while (i < n)
 	{
-		dest[i] = src;
+		dest[i] = c;
 		i++;
 	}
 	return (dest);
