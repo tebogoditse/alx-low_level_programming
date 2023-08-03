c24cb7d8e4d8b98438816de6819d04da22d64773
tebogoditse <tebogoditse2@gmail.com>
2023-08-03 22:24:15 +0200

    *

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include "main"
+
+int	_sqrt(int n, int y)
+{
+	if (n > y / 2)
+		return (-1);
+	if (n * n == y)
+		return (n);
+	return (_sqrt(n + 1, y));
+}
+
+int	_sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (_sqrt(0, n));
+}
