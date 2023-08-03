c92cc82706fe553cda77b6af36788704dffb0e72
tebogoditse <tebogoditse2@gmail.com>
2023-08-03 21:30:50 +0200

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,7 +8,12 @@
 
 void	_puts_recursion(char *s)
 {
-	_putchar(s[0]);
-	_puts_recursion(s + 1);
+	if (*s != '\0')
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 
 }
