7705801f1ba3a29085cd9199976b2d9a2746d1ee
tebogoditse <tebogoditse2@gmail.com>
2023-08-03 21:57:24 +0200

    3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,13 +8,13 @@
 
 int	factorial(int n)
 {
-	int	factorial;
+	int	fact;
 
 	if (n < 0)
 		return (-1);
 	if (n == 0)
 		return (1);
 
-	factorial = n * factorial(n - 1);
-	return (factorial);
+	fact = n * fact(n - 1);
+	return (fact);
 }
