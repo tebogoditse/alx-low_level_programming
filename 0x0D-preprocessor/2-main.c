40602804bd45bc6aa8b2a22b812bd6e674dc2bbe
tebogoditse <tebogoditse2@gmail.com>
2024-01-22 17:45:29 +0200

    main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+
+/**
+ * ft_putchar - prints a single character
+ * @c: character to print
+ * Return: no return
+ */
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+/**
+ * ft_putstr - prints a string
+ * @str: string to print
+ * Return: no return
+ */
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	ft_putstr(__FILE__);
+	ft_putchar('\n');
+	return (0);
+}
