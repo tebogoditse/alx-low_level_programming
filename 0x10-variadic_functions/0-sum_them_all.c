2bbbe0439faaa7f9fcb5b55b3265d784f3ba8be6
tebogoditse <tebogoditse2@gmail.com>
2024-01-26 02:32:28 +0200

    sum the all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sums all the arguments
+ * @n: number of arguments
+ * Return: the sum of the numbers passed
+ */
+int	sum_them_all(const unsigned int n, ...)
+{
+	unsigned int	sum;
+	va_list ap;
+	unsigned int	i;
+
+	va_start(ap, n);
+	sum = 0;
+	i = 0;
+	while (i < n)
+	{
+		sum = sum + va_arg(ap, int);
+		i++;
+	}
+	va_end(ap);
+
+	return (sum);
+}
