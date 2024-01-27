d3d78d7ac2328e88820e4995f07a1924ca09962a
tebogoditse <tebogoditse2@gmail.com>
2024-01-27 02:58:58 +0200

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: string to print
+ * @f: function to use
+ * Return: no return (void)
+ */
+void	print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL || name == NULL)
+		return;
+	f(name);
+}
