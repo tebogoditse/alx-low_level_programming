50941f0d9854cd0bda513f3560259ff9fbb59026
tebogoditse <tebogoditse2@gmail.com>
2024-01-27 03:28:19 +0200

    modify iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,12 @@
 #include "function_pointers.h"
 
+/**
+ * array_iterator - executes a function on each element of the array
+ * @array: array to iterate through
+ * @size: size of the array
+ * @action: function to use
+ * Return: no return (void)
+ */
 void	array_iterator(int *array, size_t size, void (*action)(int))
 {
 	if (!array || !action || !size)
